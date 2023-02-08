83f73a6eb7f74816c664114e67ffebaf492854d8
gls245@cornell.edu <gls245@cornell.edu>
2023-02-08 10:20:46 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import *
+from whitenoise import WhiteNoise
+
+app = Flask(__name__)
+app.wsgi_app = WhiteNoise(app.wsgi_app,
+                          root='static/',
+                          prefix='static/',
+                          # index_file='index.htm',
+                          autorefresh=True)
+
+
+@app.route('/', methods=['GET'])
+def index():
+
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(threaded=True, port=5000)
